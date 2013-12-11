9081585dddd13c5ec946c7916f12a21def836bee
Slavek Kabrda <bkabrda@redhat.com>
2013-12-11 13:11:14 +0100

    Ignore scripts that start with '..', ignore original RECORD

--- rewheel/__init__.py
+++ rewheel/__init__.py
@@ -18,8 +18,9 @@ def run():
 
 def rewheel_from_record(record_path):
     record_contents = open(record_path).read()
-    recfiles = get_records_to_pack(record_contents)
     site_dir = os.path.dirname(os.path.dirname(record_path))
+    record_relpath = record_path[len(site_dir):].strip(os.path.sep)
+    recfiles = get_records_to_pack(record_contents, record_relpath)
 
     new_wheel = zipfile.ZipFile('foo.zip', mode='w', compression=zipfile.ZIP_DEFLATED)
     # we need to write a new record with just the files that we will write,
@@ -30,22 +31,27 @@ def rewheel_from_record(record_path):
         new_record_lines.append(','.join([f, sha_hash,size]))
 
     # rewrite the old wheel file with a new computed one
-    record_relpath = record_path[len(site_dir):].strip(os.path.sep)
     new_wheel.writestr(record_relpath, '\n'.join(new_record_lines))
 
     new_wheel.close()
 
     return new_wheel.filename
 
-def get_records_to_pack(record_contents):
+def get_records_to_pack(record_contents, record_relpath):
     lines = []
     for l in record_contents.splitlines():
         spl = l.split(',')
         if len(spl) == 3:
-            # ignore abs paths (binaries) and pyc+pyo files
+            # ignore:
+            # - abs paths, paths with ".." (entry points),
+            # - pyc+pyo files
+            # - the old RECORD file
+            # TODO: is there any better way to recognize an entry point?
             if not os.path.isabs(spl[0]) and \
+               not spl[0].startswith('..') and \
                not spl[0].endswith('.pyc') and \
-               not spl[0].endswith('.pyo'):
+               not spl[0].endswith('.pyo') and \
+               spl[0] != record_relpath:
                 lines.append(spl)
         else:
             pass # bad RECORD or empty line
