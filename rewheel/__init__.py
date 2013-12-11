4361f053330beda02d4b31c2c281107fb1bab54e
Slavek Kabrda <bkabrda@redhat.com>
2013-12-11 13:15:41 +0100

    Also return the omitted files from original RECORD.
    They will be usable for writing foo.data/scripts, etc.

--- rewheel/__init__.py
+++ rewheel/__init__.py
@@ -20,13 +20,13 @@ def rewheel_from_record(record_path):
     record_contents = open(record_path).read()
     site_dir = os.path.dirname(os.path.dirname(record_path))
     record_relpath = record_path[len(site_dir):].strip(os.path.sep)
-    recfiles = get_records_to_pack(record_contents, record_relpath)
+    to_write, to_omit = get_records_to_pack(record_contents, record_relpath)
 
     new_wheel = zipfile.ZipFile('foo.zip', mode='w', compression=zipfile.ZIP_DEFLATED)
     # we need to write a new record with just the files that we will write,
     # e.g. not binaries and *.pyc/*.pyo files
     new_record_lines = []
-    for f, sha_hash, size in recfiles:
+    for f, sha_hash, size in to_write:
         new_wheel.write(os.path.join(site_dir, f), arcname=f)
         new_record_lines.append(','.join([f, sha_hash,size]))
 
@@ -38,21 +38,22 @@ def rewheel_from_record(record_path):
     return new_wheel.filename
 
 def get_records_to_pack(record_contents, record_relpath):
-    lines = []
+    to_write = []
+    to_omit = []
     for l in record_contents.splitlines():
         spl = l.split(',')
         if len(spl) == 3:
-            # ignore:
+            # new record will omit (or write differently):
             # - abs paths, paths with ".." (entry points),
             # - pyc+pyo files
             # - the old RECORD file
             # TODO: is there any better way to recognize an entry point?
-            if not os.path.isabs(spl[0]) and \
-               not spl[0].startswith('..') and \
-               not spl[0].endswith('.pyc') and \
-               not spl[0].endswith('.pyo') and \
+            if os.path.isabs(spl[0]) or spl[0].startswith('..') or \
+               spl[0].endswith('.pyc') or spl[0].endswith('.pyo') or \
                spl[0] != record_relpath:
-                lines.append(spl)
+                to_omit.append(spl)
+            else:
+                to_write.append(spl)
         else:
             pass # bad RECORD or empty line
-    return lines
+    return to_write, to_omit
